f67cd02184a8a0b5cc96bb50d2f31195a44f7697
Valentin Ochs <a@0au.de>
2014-01-09 00:31:57 +0100

    [feature/2151-targets] DMV7008: Fixed comments

--- c/include/mbug_2151_targets.h
+++ c/include/mbug_2151_targets.h
@@ -5,8 +5,8 @@ typedef char** tokenize_state_t;
 typedef void (target_parse_func_type)(tokenize_state_t *);
 
 enum {
-	MBUG_2151_DMV7008_ON  = 0,
-	MBUG_2151_DMV7008_OFF = 1,
+	MBUG_2151_DMV7008_OFF = 0,
+	MBUG_2151_DMV7008_ON  = 1,
 	MBUG_2151_DMV7008_INC = 2,
 	MBUG_2151_DMV7008_DEC = 3
 };
